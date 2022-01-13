048aab909756b93c585a003146ef77c35dc03e3d
Genilson Silva <genilsonsilvagsc@gmail.com>
2022-01-12 23:58:13 -0300

    Criação do projeto

---
+++ backend/src/main/java/com/genilsongsc/ListenMe/ListenMeApplication.java
@@ -0,0 +1,13 @@
+package com.genilsongsc.ListenMe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListenMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListenMeApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/genilsongsc/ListenMe/ListenMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.genilsongsc.ListenMe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListenMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
